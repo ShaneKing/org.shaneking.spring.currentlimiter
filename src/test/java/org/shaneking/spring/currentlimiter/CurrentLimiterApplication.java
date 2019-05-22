18c2bb6a1a27057fd9ff53f237520cffbb791798
ShaneKing <github@azolla.org>
2019-05-23 00:03:06 +0800

    format

---
+++ src/test/java/org/shaneking/spring/currentlimiter/CurrentLimiterApplication.java
@@ -0,0 +1,17 @@
+package org.shaneking.spring.currentlimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@SpringBootApplication
+public class CurrentLimiterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CurrentLimiterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/sktest/spring/currentlimiter/SKUnit.java
@@ -0,0 +1,11 @@
+package sktest.spring.currentlimiter;
+
+import org.junit.runner.RunWith;
+import org.shaneking.spring.currentlimiter.CurrentLimiterApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CurrentLimiterApplication.class)
+public class SKUnit {
+}
